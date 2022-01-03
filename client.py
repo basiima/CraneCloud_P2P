b7965a839d0fc80063cf1ee4aea4ed26735251f6
basiima <mrbasiima@gmail.com>
2022-01-03 16:59:20 +0300

    allow users to enter message or command

--- client.py
+++ client.py
@@ -1,39 +1,39 @@
 from constants import *
 
 class Client:
-
     def __init__(self, addr):
        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
        self.s.connect((addr, PORT))
-
        self.previous_data = None
+
+       print("Client Connected succesfully!")
 
        i_thread = threading.Thread(target=self.send_data)
        i_thread.daemon = True
        i_thread.start()
 
        while True:
+        r_thread = threading.Thread(target=self.recieve_message)
+        r_thread.start()
+        r_thread.join()
 
-           r_thread = threading.Thread(target=self.recieve_message)
-           r_thread.start()
-           r_thread.join()
+        data = self.recieve_message()
 
-           data = self.recieve_message()
+        if not data:
+           print("Unable to establish connection.....")
+           break
 
-           if not data:
-               print("Unable to establish connection.....")
-               break
+        elif data[0:1] == b'\x11':
+            print("Getting Data...")
+            self.update_peers(data[1:])
 
-           elif data[0:1] == b'\x11':
-               print("Getting Data...")
-               self.update_peers(data[1:])
+        self.send_data()
+
 
     def update_peers(self, peers):
-
         p2p.peers = str(peers, "utf-8").split(',')[:-1]
+
 
     def send_disconnect_signal(self):
        print("Server Disconnected!")
@@ -42,26 +42,19 @@ class Client:
 
     def recieve_message(self):
        try:
-           print("Client Connected succesfully!")
            data = self.s.recv(SIZE)
-
-           print(data.decode("utf-8"))
-
-           print("Message got from Server is:")
-
-           if self.previous_data != data:
-               self.previous_data = data
-
+           print(data.decode())
            return data
+
        except KeyboardInterrupt:
            self.send_disconnect_signal()
 
+
     def send_data(self):
         try:
-
-            self.s.send(REQ_STRING.encode('utf-8'))
-
-
+            message = input("Please enter a message: ")
+            self.s.send(message.encode())
+            
         except KeyboardInterrupt as e:
             self.send_disconnect_signal()
             return
